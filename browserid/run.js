21adb140cc67a2dc54266744a88b6ca918a2cba9
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-10-26 10:06:31 -0600

    combine app.js and run.js for browserid server

--- browserid/run.js
+++ browserid/run.js
@@ -35,12 +35,22 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var  path = require("path"),
-fs = require("fs"),
-express = require("express"),
+const
+fs = require('fs'),
+path = require('path'),
+url = require('url'),
+wsapi = require('./lib/wsapi.js'),
+ca = require('./lib/ca.js'),
+httputils = require('./lib/httputils.js'),
+sessions = require('connect-cookie-session'),
+express = require('express'),
+secrets = require('../libs/secrets.js'),
+db = require('./lib/db.js'),
+configuration = require('../libs/configuration.js'),
+heartbeat = require('../libs/heartbeat.js'),
+substitution = require('../libs/substitute.js');
+metrics = require("../libs/metrics.js"),
 logger = require("../libs/logging.js").logger;
-
-const amMain = (process.argv[1] === __filename);
 
 const PRIMARY_HOST = "127.0.0.1";
 const PRIMARY_PORT = 62700;
@@ -49,29 +59,287 @@ var handler = require("./app.js");
 
 var app = undefined;
 
-exports.runServer = function() {
-  if (app) return;
+app = express.createServer();
 
-  app = express.createServer();
+// let the specific server interact directly with the connect server to register their middleware
+if (handler.setup) handler.setup(app);
 
-  // let the specific server interact directly with the connect server to register their middleware
-  if (handler.setup) handler.setup(app);
+logger.info("browserid server starting up");
 
-  // use the express 'static' middleware for serving of static files (cache headers, HTTP range, etc)
-  app.use(express.static(path.join(__dirname, "static")));
+// open the databse
+db.open(configuration.get('database'));
 
-  app.listen(PRIMARY_PORT, PRIMARY_HOST);
-};
+const COOKIE_SECRET = secrets.hydrateSecret('browserid_cookie', configuration.get('var_path'));
+const COOKIE_KEY = 'browserid_state';
 
-exports.stopServer = function(cb) {
-  if (!app) return;
-  app.on('close', function() {
-    cb();
-  });
-  app.close();
-  app = undefined;
+function internal_redirector(new_url, suppress_noframes) {
+  return function(req, resp, next) {
+    if (suppress_noframes)
+      resp.removeHeader('x-frame-options');
+    req.url = new_url;
+    return next();
+  };
 }
 
-// when directly invoked from the command line, we'll start the server
-if (amMain) exports.runServer();
+function router(app) {
+  app.set("views", __dirname + '/views');
 
+  app.set('view options', {
+    production: configuration.get('use_minified_resources')
+  });
+
+  // this should probably be an internal redirect
+  // as soon as relative paths are figured out.
+  app.get('/sign_in', function(req, res, next ) {
+    metrics.userEntry(req);
+    res.render('dialog.ejs', {
+      title: 'A Better Way to Sign In',
+      layout: 'dialog_layout.ejs',
+      useJavascript: true,
+      production: configuration.get('use_minified_resources')
+    });
+  });
+
+  app.get("/unsupported_dialog", function(req,res) {
+    res.render('unsupported_dialog.ejs', {layout: 'dialog_layout.ejs', useJavascript: false});
+  });
+
+  // simple redirects (internal for now)
+  app.get('/register_iframe', internal_redirector('/dialog/register_iframe.html',true));
+
+  // Used for a relay page for communication.
+  app.get("/relay", function(req,res, next) {
+    // Allow the relay to be run within a frame
+    res.removeHeader('x-frame-options');
+    res.render('relay.ejs', {
+      layout: false,
+      production: configuration.get('use_minified_resources')
+    });
+  });
+
+
+  app.get('/', function(req,res) {
+    res.render('index.ejs', {title: 'A Better Way to Sign In', fullpage: true});
+  });
+
+  app.get("/signup", function(req, res) {
+    res.render('signup.ejs', {title: 'Sign Up', fullpage: false});
+  });
+
+  app.get("/forgot", function(req, res) {
+    res.render('forgot.ejs', {title: 'Forgot Password', fullpage: false, email: req.query.email});
+  });
+
+  app.get("/signin", function(req, res) {
+    res.render('signin.ejs', {title: 'Sign In', fullpage: false});
+  });
+
+  app.get("/about", function(req, res) {
+    res.render('about.ejs', {title: 'About', fullpage: false});
+  });
+
+  app.get("/tos", function(req, res) {
+    res.render('tos.ejs', {title: 'Terms of Service', fullpage: false});
+  });
+
+  app.get("/privacy", function(req, res) {
+    res.render('privacy.ejs', {title: 'Privacy Policy', fullpage: false});
+  });
+
+  app.get("/verify_email_address", function(req, res) {
+    res.render('verifyuser.ejs', {title: 'Complete Registration', fullpage: true, token: req.query.token});
+  });
+
+  app.get("/add_email_address", function(req,res) {
+    res.render('verifyemail.ejs', {title: 'Verify Email Address', fullpage: false});
+  });
+
+  // REDIRECTS
+  REDIRECTS = {
+    "/manage": "/",
+    "/users": "/",
+    "/users/": "/",
+    "/primaries" : "/developers",
+    "/primaries/" : "/developers",
+    "/developers" : "https://github.com/mozilla/browserid/wiki/How-to-Use-BrowserID-on-Your-Site"
+  };
+
+  // set up all the redirects
+  // oh my watch out for scope issues on var url - closure time
+  for (var url in REDIRECTS) {
+    (function(from,to) {
+      app.get(from, function(req, res) {
+        res.redirect(to);
+      });
+    })(url, REDIRECTS[url]);
+  }
+
+  // register all the WSAPI handlers
+  wsapi.setup(app);
+
+  // setup health check / heartbeat
+  heartbeat.setup(app);
+
+  // the public key
+  app.get("/pk", function(req, res) {
+    res.json(ca.PUBLIC_KEY.toSimpleObject());
+  });
+
+  // vep bundle of JavaScript
+  app.get("/vepbundle", function(req, res) {
+    fs.readFile(__dirname + "/../node_modules/jwcrypto/vepbundle.js", function(error, content) {
+      if (error) {
+        res.writeHead(500);
+        res.end("oops");
+        console.log(error);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(content);
+        res.end();
+      }
+    });
+  });
+
+  app.get('/code_update', function(req, resp, next) {
+    logger.warn("code updated.  shutting down.");
+    process.exit();
+  });
+};
+
+// request to logger, dev formatted which omits personal data in the requests
+server.use(express.logger({
+  format: 'dev',
+  stream: {
+    write: function(x) {
+      logger.info(typeof x === 'string' ? x.trim() : x);
+    }
+  }
+}));
+
+// over SSL?
+var overSSL = (configuration.get('scheme') == 'https');
+
+server.use(express.cookieParser());
+
+var cookieSessionMiddleware = sessions({
+  secret: COOKIE_SECRET,
+  key: COOKIE_KEY,
+  cookie: {
+    path: '/wsapi',
+    httpOnly: true,
+    // IMPORTANT: we allow users to go 1 weeks on the same device
+    // without entering their password again
+    maxAge: configuration.get('authentication_duration_ms'),
+    secure: overSSL
+  }
+});
+
+// cookie sessions && cache control
+server.use(function(req, resp, next) {
+  // cookie sessions are only applied to calls to /wsapi
+  // as all other resources can be aggressively cached
+  // by layers higher up based on cache control headers.
+  // the fallout is that all code that interacts with sessions
+  // should be under /wsapi
+  if (/^\/wsapi/.test(req.url)) {
+    // explicitly disallow caching on all /wsapi calls (issue #294)
+    resp.setHeader('Cache-Control', 'no-cache, max-age=0');
+
+    // we set this parameter so the connect-cookie-session
+    // sends the cookie even though the local connection is HTTP
+    // (the load balancer does SSL)
+    if (overSSL)
+      req.connection.proxySecure = true;
+
+    return cookieSessionMiddleware(req, resp, next);
+
+  } else {
+    return next();
+  }
+});
+
+// verify all JSON responses are objects - prevents regression on issue #217
+server.use(function(req, resp, next) {
+  var realRespJSON = resp.json;
+  resp.json = function(obj) {
+    if (!obj || typeof obj !== 'object') {
+      logger.error("INTERNAL ERROR!  *all* json responses must be objects");
+      throw "internal error";
+    }
+    realRespJSON.call(resp, obj);
+  };
+  return next();
+});
+
+app.use(express.bodyParser());
+
+// Check CSRF token early.  POST requests are only allowed to
+// /wsapi and they always must have a valid csrf token
+app.use(function(req, resp, next) {
+  // only on POSTs
+  if (req.method == "POST") {
+    var denied = false;
+    if (!/^\/wsapi/.test(req.url)) { // post requests only allowed to /wsapi
+      denied = true;
+      logger.warn("CSRF validation failure: POST only allowed to /wsapi urls.  not '" + req.url + "'");
+    }
+
+    if (req.session === undefined) { // there must be a session
+      denied = true;
+      logger.warn("CSRF validation failure: POST calls to /wsapi require an active session");
+    }
+
+    // the session must have a csrf token
+    if (typeof req.session.csrf !== 'string') {
+      denied = true;
+      logger.warn("CSRF validation failure: POST calls to /wsapi require an csrf token to be set");
+    }
+
+    // and the token must match what is sent in the post body
+    if (req.body.csrf != req.session.csrf) {
+      denied = true;
+      // if any of these things are false, then we'll block the request
+      logger.warn("CSRF validation failure, token mismatch. got:" + req.body.csrf + " want:" + req.session.csrf);
+    }
+
+    if (denied) return httputils.badRequest(resp, "CSRF violation");
+
+  }
+  return next();
+});
+
+// a tweak to get the content type of host-meta correct
+app.use(function(req, resp, next) {
+  if (req.url === '/.well-known/host-meta') {
+    resp.setHeader('content-type', 'text/xml');
+  }
+  next();
+});
+
+// Strict Transport Security
+app.use(function(req, resp, next) {
+  if (overSSL) {
+    // expires in 30 days, include subdomains like www
+    resp.setHeader("Strict-Transport-Security", "max-age=2592000; includeSubdomains");
+  }
+  next();
+});
+
+// prevent framing
+app.use(function(req, resp, next) {
+  resp.setHeader('x-frame-options', 'DENY');
+  next();
+});
+
+// add middleware to re-write urls if needed
+configuration.performSubstitution(server);
+
+// add the actual URL handlers other than static
+router(server);
+
+// use the express 'static' middleware for serving of static files (cache headers, HTTP range, etc)
+app.use(express.static(path.join(__dirname, "static")));
+
+app.listen(PRIMARY_PORT, PRIMARY_HOST, function() {
+  console.log("bound to", app.address().address, "/", app.address().port);
+});
